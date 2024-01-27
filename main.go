1e3cb0920b5c025089be21a5aa4add03f9f467fa
Guruprasad Bhagat <guruprasad.bhagat@brightlinkinfotechnologies.com>
2024-01-27 14:50:51 +0530

    Simple Slack Bot Project

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/shomali11/slacker"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
+func main() {
+
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-6468537462641-6540655058563-G3iTqGR5E5qgfHw8qJchKny4")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A06FG5XKYSK-6553393222641-e4973f9eca9cf30eef91e25774faca2f9a5929d1004558e5a7e0919c115afe74")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+	bot.Command("ping", &slacker.CommandDefinition{
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			response.Reply("Pong")
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
